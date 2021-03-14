9b2fdd85080e164440e23fff85c2b03d455d332f
Hasan Fırat <52783312+hasanfirat@users.noreply.github.com>
2021-03-15 00:02:53 +0200

    Add files via upload

--- javacode.js
+++ javacode.js
@@ -1,5 +1,7 @@
             var cimen_sayaci = 0;
+            var yildiz_sayaci = 0;
             var kaktus_sayaci = 0;
+            var bulut_sayaci = 0;
             var dino_animasyon_sayaci = 1;
             var dino_geberirse = 1;
             var yesil_dino = document.getElementById("yesil_dino");
@@ -12,40 +14,27 @@
             var jump = false;
             var gebermis_dinonun_yere_dusmesi = false;
             var score = 0;
+            var sun = true;
             var renk_karar_yapisi = false;
-            var dongu1 = setInterval(cimen_uretme, 50);
-            var dongu0 = setInterval(cimen_hareketke, 30);
+            var moon_konum = 150;
+            var sun_konum = 0;
+            var sun_have = true;
+            var sayac = 0;
+            var arazi_renk_rgb = 200;
+            var dongu1 = setInterval(gunes_ay_dongu, 1000);
             var dongu2 = setInterval(kaktus_uretme, 5000);
             var dongu3 = setInterval(kaktus_hareket_ettirme, 30);
             var dongu4 = setInterval(dino_hareketke, 70);
             var dongu5 = setInterval(durum_yapisi, 30);
             var dongu6 = setInterval(ziplama_fonksiyonu, 30);
             var dongu7 = setInterval(point_y, 100);
+            var dongu9 = setInterval(bulut_uretme, 4000);
+            var dongu10 = setInterval(bulut_hareket_ettirme, 50);
 
             function point_y()
             {
                 score++;
                 document.getElementById("point").innerHTML = "score " + score;
-            }
-
-            function cimen_uretme()
-            {
-                var cimen_konum_top = Math.floor(Math.random() * 97);
-                cimen_sayaci++;
-                document.getElementById("zemin").innerHTML += "<div class='alanda_cimen' style='top:"+ cimen_konum_top + "px;left:996px;'></div>";
-            }
-
-            function cimen_hareketke()
-            {
-                var z = document.querySelectorAll(".alanda_cimen");
-                var i = 0;
-                while (i < z.length)
-                {
-                    var yeni_konum = z[i].style.left.slice(0 , (z[i].style.left.length-2)) - ( 7 + (z[i].style.top.slice(0 , (z[i].style.top.length-2)) /25) ) ;
-                    z[i].style.left = yeni_konum + "px";
-                    if(yeni_konum < 0){z[i].remove();}                    
-                    i++;
-                }
             }
 
             function kaktus_uretme()
@@ -88,8 +77,7 @@
                     yesil_dino.src = "dino_png/dead/dead" + dino_geberirse + ".png";
                     dino_geberirse++;
                     if(dino_geberirse >= 9 )
-                    {
-                        
+                    {                        
                         clearInterval(dongu4);                        
                         clearInterval(dongu5);
                         clearInterval(dongu6);
@@ -101,10 +89,11 @@
                         yesil_dino.style.top = dino_konumu_top+"px";
                         if(dino_konumu_top >= 70)
                         {
-                            clearInterval(dongu1);
-                            clearInterval(dongu0);
                             clearInterval(dongu2);
                             clearInterval(dongu3);
+                            clearInterval(dongu9);
+                            clearInterval(dongu10);
+                            clearInterval(dongu1);
                             clearInterval(geberince_dusme);
                             var resim = document.createElement("img");
                             resim.src = "game_over.png";
@@ -169,4 +158,122 @@
                         jump=false;
                     }
                 }                
+            }
+
+            function bulut_uretme()
+            {
+                var bulut_resmi = Math.floor(Math.random() * 12);
+                document.getElementById("arazi").innerHTML += "<img src='sky/cloud/cloud"+ bulut_resmi +".png' id='bulut_"+ bulut_sayaci + "' style='top:20px;left:996px; position: absolute;' width='20' height='20' class='bulut'>";
+                bulut_sayaci++;                
+            }
+            function bulut_hareket_ettirme()
+            {
+                var k_y = document.querySelectorAll(".bulut");
+                var l_y = 0;
+
+                while (l_y < k_y.length)
+                {
+                    var yeni_konumccc_y = k_y[l_y].style.left.slice(0,(k_y[l_y].style.left.length-2)) - 5 ;
+                    if(yeni_konumccc_y < -50){k_y[l_y].remove();}
+                    k_y[l_y].style.left = yeni_konumccc_y + "px";
+                    l_y++;
+                }
+            }
+            
+            function gunes_ay_dongu()
+            {
+                sayac = sayac + 10;
+                if(sun_have == false && (sayac % 200) == 0)
+                {
+                    sun_have = true;
+                    gunes_yukselir_adim1();
+                }
+                else if(sun_have == true && (sayac % 200) == 0)
+                {
+                    sun_have = false;
+                    ay_yukselir_adim1();
+                }
+            }
+
+            function gunes_yukselir_adim1()
+            {
+                var dongu_gunes_1 = setInterval(function gunes_yukselir_ay_duser_1()
+                {                
+                    moon_konum = moon_konum + 5;
+                    document.getElementById("moon").style.top = moon_konum + "px";                
+                if(moon_konum >= 150)
+                {
+                    clearInterval(dongu_gunes_1);
+                    gunduze_gecis();
+                    gunes_yukselir_adim2();
+                }                
+                }, 50);
+            }
+
+            function gunes_yukselir_adim2()
+            {
+                var dongu_gunes_2 = setInterval(function gunes_yukselir_ay_duser_2()
+                {
+                    sun_konum = sun_konum - 5;
+                    document.getElementById("sun").style.top = sun_konum + "px";
+                    if(sun_konum <= 0 && moon_konum >= 150)
+                {
+                    clearInterval(dongu_gunes_2);
+                }                
+                }, 50);
+            }
+            
+            function ay_yukselir_adim1()
+            {
+                var dongu_ay_1 = setInterval(function gunes_duser_ay_yukselir_1()
+                {
+                sun_konum = sun_konum + 5;
+                document.getElementById("sun").style.top = sun_konum + "px";
+                if(sun_konum >= 150)
+                {
+                    clearInterval(dongu_ay_1);
+                    geceye_gecis();
+                    ay_yukselir_adim2();
+                }
+                }, 50);
+            }
+
+            function ay_yukselir_adim2()
+            {
+                var dongu_ay_2 = setInterval(function gunes_duser_ay_yukselir_2()
+                {
+                moon_konum = moon_konum - 5;
+                document.getElementById("moon").style.top = moon_konum + "px";
+                if(moon_konum <= 0 && sun_konum >= 150)
+                {
+                    clearInterval(dongu_ay_2);
+                }
+                }, 50);
+            }
+
+            function gunduze_gecis()
+            {
+                var gunduze_geciyoruz = setInterval(function gunduze_gecme_fonk()
+                {
+                    arazi_renk_rgb = arazi_renk_rgb + 5;
+                    document.getElementById("arazi").style.backgroundColor = "rgb(" + arazi_renk_rgb + ", " + arazi_renk_rgb + ", " + arazi_renk_rgb + ")";
+                    if(arazi_renk_rgb == 200)
+                    {
+                        clearInterval(gunduze_geciyoruz);
+                    }          
+                }, 50); 
+                
+            }
+
+            function geceye_gecis()
+            {
+                var geceye_geciyoruz = setInterval(function geceye_gecme_fonk()
+                {
+                    arazi_renk_rgb = arazi_renk_rgb - 5;
+                    document.getElementById("arazi").style.backgroundColor = "rgb(" + arazi_renk_rgb + ", " + arazi_renk_rgb + ", " + arazi_renk_rgb + ")";
+                    if(arazi_renk_rgb == 100)
+                    {
+                        clearInterval(geceye_geciyoruz);
+                    }          
+                }, 50); 
             }
